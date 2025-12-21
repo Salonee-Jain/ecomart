0471d01b546719e2451e7d4bad4f25ada2791268
Saloni <z5426263@ad.unsw.edu.au>
2025-12-21 22:58:04 +1100

    feat: Add admin protection to get all payments route

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/create-intent", protect, createPaymentIntent);
 router.post("/confirm/:paymentIntentId", protect, confirmPaymentIntent);
 router.get("/:id", protect, getPaymentById);
+router.get("/", protect, admin, getAllPayments);
 
 export default router;
